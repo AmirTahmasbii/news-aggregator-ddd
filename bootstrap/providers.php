d1dc1940179037f0980b031dee6169ff45f8ebd4
AmirTahmasbii <amirtahmasbi91@gmail.com>
2025-01-08 13:12:57 +0100

    change mvc to ddd

---
+++ bootstrap/providers.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    Application\Providers\ApplicationServiceProvider::class,
+    Domain\Providers\DomainServiceProvider::class,
+    Infrastructure\Providers\InfrastructureServiceProvider::class,
+];
