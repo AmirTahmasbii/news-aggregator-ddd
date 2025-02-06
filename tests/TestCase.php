71042ef5aa36b2df0d5139587ce2ec8e881bb453
Amir Tahmasbi <86706838+AmirTahmasbii@users.noreply.github.com>
2025-02-06 11:16:37 +0100

    Update TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,13 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    use CreateApplication;
+
+    protected function setUp()
+    {
+        parent::setUp();
+        
+        $this->withoutVite();
+    }
     //
 }
